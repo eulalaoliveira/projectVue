d5f207a4b001179fc427964243ca44d7b3d1200d
Larissa Oliveira <larissa.brazil@live.com>
2022-12-02 15:23:27 +0000

    input forms reusable component

--- vue-project/src/components/Pessoa.vue
+++ vue-project/src/components/Pessoa.vue
@@ -2,15 +2,18 @@
     <div>
         <h2>Essa é a descrição da pessoa: {{ nome }} </h2>
         <Info />
+        <Form />
     </div>
 </template>
 <script>
     import Info from "./Info.vue"
+    import Form from "./Form.vue"
 
     export default {
         name: 'Pessoa',
         components: {
-            Info
+            Info,
+            Form
         },
         data() {
             return {
