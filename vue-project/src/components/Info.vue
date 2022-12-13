60c808e52eda6c6e63c8da575b9506cbb0a329f5
Larissa Oliveira <larissa.brazil@live.com>
2022-12-13 15:24:51 -0300

    rendenring arrays

--- vue-project/src/components/Info.vue
+++ vue-project/src/components/Info.vue
@@ -2,11 +2,17 @@
     <div>
         <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
         <p v-else>Estou em busca de novas oportunidades!</p>
-        <p>Utilizo as seguintes tecnologias:</p>
+        <p>Utilizo as seguintes tecnologias para back-end:</p>
         <ul>
-            <li>JavaScript</li>
-            <li>PHP</li>
-            <li>Python</li>
+            <li v-for="(technology, index) in backend_technologies" v-bind:key="index">
+                {{ technology }}
+            </li>
+        </ul>
+        <p>Utilizo as seguintes tecnologias para front-end:</p>
+        <ul>
+            <li v-for="(technology) in frontend_technologies" :key="technology.id">
+                {{ technology.language }}
+            </li>
         </ul>
         <div>
             <button @click="showEmail"> {{ textoBotao }} </button>
@@ -30,7 +36,13 @@ import Picture from "../components/Picture.vue"
                 mostrar_email: false,
                 email: "larissa@gmail.com",
                 meu_link: "https://google.com",
-                textoBotao: "Mostrar e-mail"
+                textoBotao: "Mostrar e-mail",
+                backend_technologies: ["JavaScript", "PHP", "Python"],
+                frontend_technologies: [
+                    {id: 1, language: "HTML"},
+                    {id: 2, language: "CSS"},
+                    {id: 3, language: "Vue"}
+                ]
             }
         },
         methods: {
